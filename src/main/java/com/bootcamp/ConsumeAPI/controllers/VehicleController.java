65d6c146d7d460cd64a136f71946f1135de908af
FikriNajib <rifik91@gmail.com>
2020-03-02 10:39:56 +0700

    Vehicle Lagi bisa Save

---
+++ src/main/java/com/bootcamp/ConsumeAPI/controllers/VehicleController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bootcamp.ConsumeAPI.controllers;
+
+import com.bootcamp.ConsumeAPI.entities.Employee;
+import com.bootcamp.ConsumeAPI.entities.Vehicle;
+import com.bootcamp.ConsumeAPI.services.VehicleService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author FIKRI-PC
+ */
+@RequestMapping(value="vehicle")
+@Controller
+public class VehicleController {
+     @Autowired
+    VehicleService service;
+    
+    @GetMapping("")
+    public String getAll(Model model){
+        model.addAttribute("vehicles",service.getAll()) ;
+          return "vehicle";
+    }
+//    @RequestMapping
+    @PostMapping("save")
+    public String save(@Valid Vehicle vehicle){
+            vehicle.setPhotoStnk("poto");
+            Employee employee=new Employee();
+            employee.setId("16163");
+            vehicle.setEmployee(employee);
+        service.save(vehicle);
+        return("redirect:/vehicle");
+    }
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable("id") String id){
+    
+        model.addAttribute("vehicle",service.getById(id));
+        model.addAttribute("vehicles",service.getAll());
+        return "vehicle";
+    }
+  
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") String id){
+    
+        service.delete(id);
+        return("redirect:/");
+    }
+  
+}
---
+++ src/main/java/com/bootcamp/ConsumeAPI/services/EmployeeService.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bootcamp.ConsumeAPI.services;
+
+import com.bootcamp.ConsumeAPI.entities.Employee;
+import com.bootcamp.ConsumeAPI.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author FIKRI-PC
+ */
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee getById(String id){
+        Employee employee=employeeRepository.findById(id).get();
+
+        return employee;
+
+    }
+
+    public Employee save(Employee employee){
+        employeeRepository.save(employee);
+
+        return employee;
+    }
+    
+}
